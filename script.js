ac51ee4d9fe56839a733d749e5f7edf49175bed0
MajorMetal <MajorMetal@clownfart>
2014-07-31 14:39:51 -0700

    angular working

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
-var app = angular.module("manApp", []);
-	app.controller('manCtrl', function($scope) {
+var app = angular.module("manApp", ["firebase"]);
+	app.controller('manCtrl', function($scope, $firebase) {
+var ref = new Firebase('https://glaring-fire-920.firebaseio.com/');
+var sync = $firebase(ref);
+	    
+	    $scope.tiles = sync.$asArray();
 
-		$scope.manTag = "";
-		$scope.manImage = "";
-
-		$scope.men = [
-
-			{	tag:"Matt",
-				image:"http://photos-h.ak.fbcdn.net/hphotos-ak-xpf1/t1.0-0/p200x200/1982256_829204587093166_3617512461077262572_n.jpg"
-
-			},
-			{
-				tag:"",
-				image:""
-			},
-			{	tag:"",
-				image:""
-			},
-			{
-				tag:"",
-				image:""
-			},	
-			{
-				tag:"",
-				image:"" 
-
-			},
-
-		]
+		
+		$scope.addMan = function(tag, image, url){
+			$scope.tiles.$add({tag: tag, image: image, url: url,});
+		}
 	});
